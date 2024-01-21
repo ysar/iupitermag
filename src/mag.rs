462380ff70b75bf7280acad7aecb6198e0289ad8
ysar <31999239+ysar@users.noreply.github.com>
2024-01-21 00:55:08 -0500

    first commit

---
+++ src/mag.rs
@@ -0,0 +1,9 @@
+//extern crate ndarray;
+
+use crate::legendre;
+
+
+// pub fn calc_field(
+//     r : f64, theta : f64, phi : f64, 
+//     g : Array<f64, Ix2>, h : Array<f64, Ix2>
+// )
---
+++ src/main.rs
@@ -0,0 +1,42 @@
+extern crate ndarray;
+extern crate lazy_static;
+
+//use crate::legendre;
+use std::f64::consts::PI;
+
+pub mod legendre;
+pub mod mag;
+pub mod coefficients;
+
+#[derive(Debug)]
+struct Node {
+    pos   : [f64; 3],
+    b_val : [f64; 3],
+}
+
+
+fn main() {
+    let some_node = Node {
+        pos   : [1., 2., 3.],
+        b_val : [0., 2.2, -1.],
+    };
+
+    // Initialize a vector of nodes
+    let mut vec : Vec<Node> = Vec::new();
+
+    vec.push(some_node);
+
+    println!("{:?}", vec);
+
+    let theta : f64 = 16. * PI / 180.;
+    let (p, p_diff) = legendre::assoc_legendre_poly(theta, 3);
+
+    println!("{}", p);
+    println!("{}", p_diff);
+
+    let type_field = coefficients::InternalField::JRM33;
+    let (g, h) = type_field.get_coefficients();
+
+    println!("{:?}", g);
+    // p[[0, 0]] = 1.
+}
