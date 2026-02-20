f8466f2ddc825aef727d309b674a75cef039e256
ysar <31999239+ysar@users.noreply.github.com>
2026-02-20 09:30:04 -0600

    add license and one test. to add more later

--- tests/test_field.py
+++ tests/test_field.py
@@ -1,24 +1,19 @@
 import numpy as np
+
 import iupitermag
 
 
 def test_planet_field_point():
 
-    x = 10.
-    y = 0.
-    z = 0.
+    x = 10.0
+    y = 0.0
+    z = 0.0
 
-    inArr = np.array([
-        np.sqrt(x**2 + y**2 + z**2), 
-        np.arccos(z / x),
-        np.arctan2(y, x)
-        ])
+    in_arr = np.array([np.sqrt(x**2 + y**2 + z**2), np.arccos(z / x), np.arctan2(y, x)])
 
-    internalField = iupitermag.InternalField("JRM09", degree=10)
+    internal_field = iupitermag.InternalField("JRM09", degree=10)
 
-    outArr = internalField.calc_field(inArr[0], inArr[1], inArr[2])
+    out_arr = internal_field.calc_field(in_arr[0], in_arr[1], in_arr[2])
     expectedArr = np.array([-131.3754, 400.6375, -24.2054])
-    
-    assert(np.allclose(outArr, expectedArr, rtol=1e-3))
 
-
+    assert np.allclose(out_arr, expectedArr, rtol=1e-3)
